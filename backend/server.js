191100545d63f332bcfbc3f8fabe85dfb40b67bc
Juan Carlos Aragón <juank1316@gmail.com>
2021-09-04 10:47:42 -0600

    Estructura Inicial

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const port = 3000;
+const routes = require('./network/routes');
 
 const app = express();
 
 app.use(express.urlencoded());
 app.use(express.json());
+
+app.use(routes)
 
 app.listen(port,()=> {
     console.log(`App Runnig on http://localhost:${port}`);
