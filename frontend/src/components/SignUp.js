555c4e100df8a1fd461a6880a83e611284e12f7d
Juan Carlos Aragón <juank1316@gmail.com>
2021-09-07 15:23:09 -0600

    Se realizó la subido de foto al bucket publico

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,20 +1,84 @@
 import React from "react";
 import './resources/signup.css'
+import UploadService from '../services/upload-photo'
 
 class SignUp extends React.Component {
 
+    constructor(props) {
+
+        super(props);
+        this.selectFile = this.selectFile.bind(this);
+        this.upload = this.upload.bind(this);
+
+        this.state = {
+            selectedFiles: undefined,
+            currentFile: undefined,
+            progress: 0,
+            message: "",
+
+            fileInfos: [],
+        };
+    }
+
+    selectFile(event) {
+        this.setState({
+          selectedFiles: event.target.files,
+        });
+    }
+
+    upload() {
+
+        let currentFile = this.state.selectedFiles[0];
+    
+        this.setState({
+            progress: 0,
+            currentFile: currentFile,
+        });
+    
+        UploadService.upload(currentFile, (event) => {
+            this.setState({
+                progress: Math.round((100 * event.loaded) / event.total),
+            });
+        })
+        .then((response) => {
+            console.log(response)
+            this.setState({
+              //message: response.data.message,
+              selectedFiles: undefined,
+              currentFile: undefined,
+
+            });
+        })
+        .catch(() => {
+            this.setState({
+                progress: 0,
+                message: "Could not upload the file!",
+                currentFile: undefined,
+            });
+        });
+    
+        this.setState({
+          selectedFiles: undefined,
+        });
+    }
+
     render() {
+
+        const {
+            selectedFiles,
+            currentFile,
+            progress,
+            message,
+            fileInfos,
+        } = this.state;
+      
         return (
             <div>
                 <h2>SignUp</h2>
-                <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Check Password" />
-                    <input type="file" />
-                    <button>Sign up</button>
-                </form>
+                
+                    <input id="photoUploader" type="file" onChange={this.selectFile} />
+                    <button disabled={!selectedFiles} onClick={this.upload}>Sign up</button>
+                    
             </div>
         );
     }
