a3b699737a87d361b4a42621a08ac062f8fa58a3
Juan Carlos Aragón <juank1316@gmail.com>
2021-09-19 00:15:21 -0600

    hotfix agregar puerto

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(config.db.database,config.db.username,config.db.password, {
     host: config.db.host,
+    port: 3306,
     dialect: config.db.type
 });
 
